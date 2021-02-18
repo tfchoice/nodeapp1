505162b65e9fe2bfae33135245304958c79df295
tfchoice <78902023+tfchoice@users.noreply.github.com>
2021-02-18 23:45:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ http.createServer(function (req, res) {
 		}); 
 	} 
 	
-}).listen(3000, function () { 
+}).listen(process.env.PORT || 3000, function () { 
 	console.log("SERVER STARTED PORT: 3000"); 
 }); 
